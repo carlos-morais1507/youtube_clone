3ac89e928a7485dcdb5046fc4b25ef1228e641f9
c.morais <c.morais1507@gmail.com>
2023-02-10 15:19:32 -0300

    chore: Integrate Youtube API

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,13 +1,12 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from "./Sidebar";
+import { Sidebar, Videos } from "./";
 
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md:'row' } }}>
       <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx:0, md: 2 } }}>
         <Sidebar />
-
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }}>
           Copyright 2022 CMTube
         </Typography>
@@ -16,6 +15,8 @@ const Feed = () => {
         <Typography variant="h4" fontWeight='bold' mb={2} sx={{ color: 'white' }}>
           New <span style={{ color: '#F31503' }}>videos</span>
         </Typography>
+
+        <Videos videos={[]}/>
       </Box>
     </Stack>
   )
